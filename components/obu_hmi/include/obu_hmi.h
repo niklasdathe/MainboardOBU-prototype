5ae641cce8044272440672c0724086fda43bc4a9
Niklas <94556032+niklasdathe@users.noreply.github.com>
2026-08-14 14:04:10 +0200

    align HMI warning text with canonical warning model

--- components/obu_hmi/include/obu_hmi.h
+++ components/obu_hmi/include/obu_hmi.h
@@ -9,7 +9,7 @@ extern "C" {
 #endif
 
 typedef enum {OBU_HMI_WARN_NONE=0,OBU_HMI_WARN_DENM,OBU_HMI_WARN_SYSTEM} obu_hmi_warning_t;
-typedef struct {float speed_kmh;bool speed_valid;bool phone_connected;bool c5_online;bool gnss_valid;bool glosa_valid;float glosa_target_kmh;obu_hmi_warning_t warning;char warning_text[24];} obu_hmi_model_t;
+typedef struct {float speed_kmh;bool speed_valid;bool phone_connected;bool c5_online;bool gnss_valid;bool glosa_valid;float glosa_target_kmh;obu_hmi_warning_t warning;char warning_text[48];} obu_hmi_model_t;
 typedef struct obu_display_driver obu_display_driver_t;
 typedef struct {esp_err_t(*render)(obu_display_driver_t*,const obu_hmi_model_t*);esp_err_t(*set_enabled)(obu_display_driver_t*,bool);} obu_display_ops_t;
 struct obu_display_driver {const obu_display_ops_t *ops;void *ctx;};
