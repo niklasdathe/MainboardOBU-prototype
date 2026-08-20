a76c9188df0674cd5a9649513fba44e072bb11fe
Niklas <94556032+niklasdathe@users.noreply.github.com>
2026-08-20 10:20:08 +0200

    Expose LoRaWAN application rate policy in component config

--- components/obu_lorawan/include/obu_lorawan.h
+++ components/obu_lorawan/include/obu_lorawan.h
@@ -38,6 +38,8 @@ typedef struct {
     const char *nwk_key_hex;
     const char *app_key_hex;
     uint8_t join_datarate;
+    bool adr_enabled;
+    uint8_t uplink_datarate;
     uint8_t fport;
     uint16_t max_frame_bytes;
     uint8_t fragment_data_bytes;
